67904fce5fa65d64c3ef36de5a5b7b7bd62804e0
Edwin Chen <edwin14chen@gmail.com>
2017-01-20 18:54:05 -0500

    Physics 2D settings

---
+++ GlobalGameJam2017/Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+#region // Variables
+    public Rigidbody2D rb2D;
+
+    public float amplitude;
+    public float cycle;
+
+    public float Xspeed;
+    public float Yspeed;
+    public float deathTime;
+    public float frequency;
+#endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, deathTime);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void SineWaveTranformation()
+    {
+
+    }
+
+} // end class Projectile
