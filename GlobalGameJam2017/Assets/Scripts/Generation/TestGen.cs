447fcf18d926c6ebbe72c92914800d23838785c7
aidandearing <aidan.dearing@live.com>
2017-01-20 19:28:17 -0500

    Catch Up

--- GlobalGameJam2017/Assets/Scripts/Generation/TestGen.cs
+++ GlobalGameJam2017/Assets/Scripts/Generation/TestGen.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestGen : MonoBehaviour {
+public class TestGen : MonoBehaviour
+{
+    public List<GameObject> BuildingPrefabs;
+    public List<GameObject> RoadPrefabs;
+    public List<GameObject> Test;
+
+    private const int CellWidth = 10;
+    private const int CityWidth = 200;
+    private const int CityHeight = 200;
+    private const int BlockWidth = 10;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        for (int i = 0; i < CityWidth; i++) ;	
 	}
 	
 	// Update is called once per frame
