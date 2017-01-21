188b98f9487b9b312064e9038c50e9deb27b9720
Nick Whelan <nickwhelan5@gmail.com>
2017-01-20 23:11:06 -0500

    Added Player Movement

--- GlobalGameJam2017/Assets/Scripts/TopDownController.cs
+++ GlobalGameJam2017/Assets/Scripts/TopDownController.cs
@@ -1,8 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ThirdPersonController : Controller
+public class TopDownController : MonoBehaviour
 {
+    [SerializeField]
+    private float _playerID;
+    public float PlayerID { get { return _playerID; } set { _playerID = value; } }
+
+    [SerializeField]
+    private float _health;
+    public float Health { get { return _health; } set { _health = value; } }
+    [SerializeField]
+    private float _healthmax;
+    public float HealthMax { get { return _healthmax; } set { _healthmax = value; } }
+    private float healthRegenCurrent;
+    [SerializeField]
+    private Vector3 _movement;
+    public Vector3 Movement { get { return _movement; } set { _movement = value; } }
+    [SerializeField]
+    private float _movementbackstepmult;
+    public float MovementBackstepMult { get { return _movementbackstepmult; } set { _movementbackstepmult = value; } }
+    [SerializeField]
+    private float _movementsprintmult;
+    public float MovementSprintMult { get { return _movementsprintmult; } set { _movementsprintmult = value; } }
+
     [SerializeField]
     private Rigidbody rigidself;
     [SerializeField]
@@ -25,16 +46,9 @@ public class ThirdPersonController : Controller
     // Update is called once per frame
     protected  void Update()
     {
-       // base.Update();
-
-
-        Ray down = new Ray(transform.position, Vector3.down);
-        RaycastHit ground;
-        isOnGround = Physics.Raycast(down, out ground, 1.0f);
-
         // THIS NEEDS TO GO AT SOME POINT
         // This is my hacky way of shutting the damn errors up for a bit so I can debug.
-        if (Movement.magnitude > 0)
+        //if (Movement.magnitude > 0)
             HandleInput();
 
         // Line renderer code
@@ -52,15 +66,32 @@ public class ThirdPersonController : Controller
 
             // If moving forward or backward, add only the forward backward component (z)
             if (Input.GetKey(KeyCode.W))
-                moveGoal.z = Movement.z;
+                moveGoal.y = Movement.y;
+ 
             else if (Input.GetKey(KeyCode.S))
-                moveGoal.z = -Movement.z;
+                moveGoal.y = -Movement.y;
 
             // If moving sideways, add only the sideways component (x)
             if (Input.GetKey(KeyCode.A))
                 moveGoal.x = -Movement.x;
             else if (Input.GetKey(KeyCode.D))
                 moveGoal.x = Movement.x;
+            if (Input.GetAxis("L Horizontal Controller") != 0 ){
+                moveGoal.x = Input.GetAxis("L Horizontal Controller");
+            }
+            if (Input.GetAxis("L Vertical Controller") != 0)
+            {
+                moveGoal.y = Input.GetAxis("L Vertical Controller");
+            }
+            if (Input.GetAxis("R Horizontal Controller") != 0)
+            {
+                moveGoal.x = Input.GetAxis("R Horizontal Controller");
+            }
+            if (Input.GetAxis("R Vertical Controller") != 0)
+            {
+                moveGoal.y = Input.GetAxis("R Vertical Controller");
+            }
+
 
             // Normalize it
             moveGoal.Normalize();
@@ -70,18 +101,18 @@ public class ThirdPersonController : Controller
             // So if you normalize the whole mess, then restore the leading axis (z) take whatever percentage of the current move vector in comparison to the goal movement speed and use it as a
             // multiplier against the secondary axis (x) magnitude, you establish a move vector that is limited to a maximum magnitude of the leading axis.
             // Save I am ignoring y, because it is for jumping, which doesn't need any of this.
-            moveGoal.z *= Movement.z;
-            moveGoal.x *= (1 - (moveGoal.z / Movement.z)) * Movement.x;
+            moveGoal.y *= Movement.y;
+            moveGoal.x *= (1 - (moveGoal.y / Movement.y)) * Movement.x;
 
             // If the z is negative the player is moving backwards, they should be backstepping, therefore they need to use the backstepMult
-            if (moveGoal.z < 0)
-                moveGoal.z *= MovementBackstepMult;
+            if (moveGoal.y < 0)
+                moveGoal.y *= MovementBackstepMult;
 
             // Sprinting, if moving forward
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                if (moveGoal.z > 0)
-                    moveGoal.z *= MovementSprintMult;
+                if (moveGoal.y > 0)
+                    moveGoal.y *= MovementSprintMult;
             }
 
             // Then it is just a matter of using the beautiful SmoothDamp method (much like a spring, but unable to go past the goal) to figure out the best way of making the player move naturally
@@ -90,9 +121,9 @@ public class ThirdPersonController : Controller
 
         // Action logic
         // Shoot
-        if (Input.GetButton("Shoot"))
+        if (Input.GetButton("Fire1")|| Input.GetKey(KeyCode.JoystickButton0))
         {
-            BroadcastMessage("Shoot");//, SendMessageOptions.DontRequireReceiver);
+            print("A");
         }
     }
 
