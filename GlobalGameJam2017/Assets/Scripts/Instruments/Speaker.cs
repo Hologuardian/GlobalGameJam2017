2dd86369ee648d8432ec706f0836021e8efa24fa
Hologuardian <namaeskram@gmail.com>
2017-01-22 06:41:51 -0500

    Utility.

---
+++ GlobalGameJam2017/Assets/Scripts/Instruments/Speaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speaker : MonoBehaviour
+{
+    public Projectile basicAttack;
+    public Projectile chargeAttack;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
